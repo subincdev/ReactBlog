a35984b70bdf20fc0b206b6104a86656eb696e8e
subincdev <subinc.dev@gmail.com>
2022-07-02 23:19:00 +0900

    :sparkles: Index페이지에서 fetch로 데이터 props로 넘겨주기

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/header/Header";
 import Banner from "../components/banner/Banner";
+import Main from "../components/main/Main";
+
 export default function Index() {
-  return (
-    <>
-      <Header />
-      <Banner />
-    </>
-  );
+  const [applicationData, setApplicationData] = useState();
+  const [isLoaded, setIsLoaded] = useState(false);
+  console.log(applicationData);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("./data.json");
+        const data = await response.json();
+        setApplicationData(data);
+        setIsLoaded(true);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (isLoaded) {
+    return (
+      <>
+        <Header />
+        <Banner data={applicationData.blog} />
+        <Main user={applicationData.users} posts={applicationData.posts} />
+      </>
+    );
+  }
 }
