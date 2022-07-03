a1ded2000b59920d7ae8eacde43dd3d5a12df54b
subincdev <subinc.dev@gmail.com>
2022-07-03 23:58:00 +0900

    :sparkles: Button 컴포넌트 props 추가

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,16 +1,8 @@
 import React from "react";
 import * as S from "./Button.style";
 
-export default function Button() {
-  const buttonList = [
-    "Life",
-    "Style",
-    "Tech",
-    "Sport",
-    "Photo",
-    "Develop",
-    "Music",
-  ];
+export default function Button({ categories }) {
+  const buttonList = categories;
   const listItem = buttonList.map((btn, idx) => (
     <li key={idx}>
       <S.BtnCategories to="/">{btn}</S.BtnCategories>
