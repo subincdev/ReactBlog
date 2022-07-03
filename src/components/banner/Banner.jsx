dbfa2b70f032712bb4269d914d2f11bf3ff5a310
subincdev <subinc.dev@gmail.com>
2022-07-03 23:55:00 +0900

    :sparkles: Banner 컴포넌트 props 추가

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import * as S from "./Banner.style";
 
-export default function Banner() {
+export default function Banner({ data }) {
   return (
     <>
       <S.BannerDiv>
         <S.WrapDiv>
           <S.BannerContents>
-            <S.Subtext>React Blog</S.Subtext>
-            <S.Maintext>My BLOG</S.Maintext>
-            <S.Description>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              nostrum dolor provident beatae deserunt magnam, illo neque.
-              Maiores ipsum commodi dolorum beatae aliquid laboriosam est
-            </S.Description>
+            <S.Subtext>{data.subTitle}</S.Subtext>
+            <S.Maintext>{data.mainTitle}</S.Maintext>
+            <S.Description>{data.description}</S.Description>
           </S.BannerContents>
         </S.WrapDiv>
       </S.BannerDiv>
