25d9da6d24e28d3d9c5c844e30bb7415b9d185dd
subincdev <subinc.dev@gmail.com>
2022-07-04 00:37:44 +0900

    :sparkles: SnsButton 컴포넌트 props 추가

--- src/components/snsButton/SnsButton.jsx
+++ src/components/snsButton/SnsButton.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-import FacebookImg from "../../assets/Facebook.svg";
-import TwitterImg from "../../assets/Twitter.svg";
-import InstagramImg from "../../assets/Instagram.svg";
-import GitHubImg from "../../assets/Github.svg";
 import * as S from "./SnsButton.style";
-export default function SnsButton() {
-  const imgList = [
-    { imgsrc: FacebookImg, src: "https://www.facebook.com/" },
-    { imgsrc: TwitterImg, src: "https://twitter.com/home" },
-    { imgsrc: InstagramImg, src: "https://www.instagram.com/" },
-    { imgsrc: GitHubImg, src: "https://github.com/" },
-  ];
 
-  const imgListItem = imgList.map(({ imgsrc, src }, idx) => (
+export default function SnsButton({ snsBtn }) {
+  const keys = Object.keys(snsBtn);
+  const imgListItem = keys.map((name, idx) => (
     <li key={idx}>
-      <a href={src}>
-        <S.ImgComponent src={imgsrc} />
+      <a href={snsBtn[name]}>
+        <S.ImgComponent src={`../../assets/${name}.svg`} />
       </a>
     </li>
   ));
-
   return <S.StyledUl>{imgListItem}</S.StyledUl>;
 }
