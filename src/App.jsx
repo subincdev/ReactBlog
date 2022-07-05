ca5777579d432a96945a70ac6afaf0169a667f7f
subincdev <subinc.dev@gmail.com>
2022-07-05 16:26:52 +0900

    :adhesive_bandage: Index에서 App파일로 fetch 코드 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
 import Index from "./pages/Index";
-import Post from "./components/post/Post";
+// import Post from "./components/post/Post";
+import PostDetail from "./components/postDetail/PostDetail";
 import { BrowserRouter, Route } from "react-router-dom";
 import * as S from "./App.style";
-
+import React, { useState, useEffect } from "react";
+import Header from "./components/header/Header";
 function App() {
+  const [applicationData, setApplicationData] = useState();
+  const [isLoaded, setIsLoaded] = useState(false);
+  console.log(applicationData);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("./data.json");
+        const data = await response.json();
+        setApplicationData(data);
+        setIsLoaded(true);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <S.GlobalStyle />
       <BrowserRouter>
-        <Route path="/" exact component={Index} />
-        <Route path="/post" exact component={Post} />
+        <Header />
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <Index applicationData={applicationData} isLoaded={isLoaded} />
+          )}
+        />
+        <Route
+          path="/postDetail/:id"
+          exact
+          render={() => (
+            <PostDetail applicationData={applicationData} isLoaded={isLoaded} />
+          )}
+        />
       </BrowserRouter>
     </>
   );
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,34 +1,15 @@
-import React, { useState, useEffect } from "react";
-import Header from "../components/header/Header";
 import Banner from "../components/banner/Banner";
 import Main from "../components/main/Main";
 
-export default function Index() {
-  const [applicationData, setApplicationData] = useState();
-  const [isLoaded, setIsLoaded] = useState(false);
-  console.log(applicationData);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("./data.json");
-        const data = await response.json();
-        setApplicationData(data);
-        setIsLoaded(true);
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
-
+export default function Index({ isLoaded, applicationData }) {
   if (isLoaded) {
     return (
       <>
-        <Header />
         <Banner data={applicationData.blog} />
         <Main user={applicationData.users} posts={applicationData.posts} />
       </>
     );
+  } else {
+    return <div>로딩중입니다...</div>;
   }
 }
