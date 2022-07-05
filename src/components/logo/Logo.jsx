59d728af2f588e2e3bf0f2867bca4ec6c94610ed
subincdev <subinc.dev@gmail.com>
2022-07-05 16:37:30 +0900

    :bug: Logo 컴포넌트 경로 수정

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,15 +1,10 @@
-import { BrowserRouter, Route } from "react-router-dom";
-import Index from "../../pages/Index";
 import * as S from "./Logo.style";
 export default function Logo() {
   return (
     <S.StyledH1>
-      <BrowserRouter>
-        <S.StyledLink to="/home">
-          <S.StyledImg />
-        </S.StyledLink>
-        <Route path="/home" exact component={Index} />
-      </BrowserRouter>
+      <S.StyledLink to="/">
+        <S.StyledImg />
+      </S.StyledLink>
     </S.StyledH1>
   );
 }
